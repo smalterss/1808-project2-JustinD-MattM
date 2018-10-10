c2339a2462e34ec0da4ce44e1586dff30ef54d8d
Justin Donn <jld92695@gmail.com>
2018-10-09 21:23:11 -0400

    added Application pojo

---
+++ PAMS/src/main/java/com/revature/pojos/Application.java
@@ -0,0 +1,47 @@
+package com.revature.pojos;
+
+public class Application {
+
+	String birthdate;
+	String fName;
+	String lName;
+	String jobExperience;
+	String position;
+	
+	
+	
+	public Application() {
+		super();
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getJobExperience() {
+		return jobExperience;
+	}
+	public void setJobExperience(String jobExperience) {
+		this.jobExperience = jobExperience;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+}
