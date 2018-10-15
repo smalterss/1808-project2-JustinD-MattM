da3049437bfec57415fef8d0a9648ba5b5ce37e1
Justin Donn <jld92695@gmail.com>
2018-10-15 17:40:25 -0400

    added a tomcat server to java and made it so the login page can open with Http info in constructor

--- PAMS/src/main/java/com/revature/controllers/LoginController.java
+++ PAMS/src/main/java/com/revature/controllers/LoginController.java
@@ -7,21 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.revature.pojos.User;
 import com.revature.services.UserServices;
 
-@Controller
-public class LoginController {
 
+@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+public class LoginController {
+	
 	@Autowired
-	private UserServices us;
+	private UserServices us = new UserServices();
 	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String loginGet(HttpSession sess){
-		if(sess.getAttribute("user") != null) {
+		System.out.println("You made it to the GET method in Login Controller");
+		User user = UserServices.verifyUser((String) sess.getAttribute("email"), (String) sess.getAttribute("password"));
+		
+		if(user != null) {
 			return "home";
 		}
 		return "login";
--- project2/src/app/app-routing.module.ts
+++ project2/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './Components/login-page/login-page.component';
+import { HomeComponent } from './Components/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: HomeComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
